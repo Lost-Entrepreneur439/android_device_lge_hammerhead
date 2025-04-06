32ce3c5d42c2abe6201bb3f3fbb571e8833c37cc
LuK1337 <priv.luk@gmail.com>
2025-04-06 19:16:58 +0300

    hammerhead: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_hammerhead.mk
+++ lineage_hammerhead.mk
@@ -17,7 +17,6 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=hammerhead \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
-
-BUILD_FINGERPRINT := google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys
+    DeviceName=hammerhead \
+    BuildDesc="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys" \
+    BuildFingerprint=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys
