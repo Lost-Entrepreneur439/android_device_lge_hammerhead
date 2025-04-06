ebfb37d9e029bd30c51b36379c78cf627e60d1aa
Yaroslav Zviezda <acroreiser@gmail.com>
2025-04-06 19:20:48 +0300

    hammerhead: use Lineage Go configs

--- lineage_hammerhead.mk
+++ lineage_hammerhead.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_go_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
