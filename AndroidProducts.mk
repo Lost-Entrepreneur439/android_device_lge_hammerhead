e04bfb71b48f781226b594032d756598d7be4766
Yaroslav Zviezda <acroreiser@gmail.com>
2025-04-07 21:43:08 +0300

    hammerhead: configure GMS Go 2GB product variant

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hammerhead.mk
+    $(LOCAL_DIR)/lineage_hammerhead.mk \
+    $(LOCAL_DIR)/lineage_hammerhead_gms_go_2gb.mk
 
 COMMON_LUNCH_CHOICES := \
+    lineage_hammerhead_gms_go_2gb-user \
     lineage_hammerhead-user \
     lineage_hammerhead-userdebug \
     lineage_hammerhead-eng
