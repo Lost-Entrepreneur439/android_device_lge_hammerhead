7a608936b596e11bb733d766108693c6deabaf15
Yaroslav Zviezda <acroreiser@gmail.com>
2025-05-10 22:21:10 +0300

    hammerhead: Reduce graphics memory consumption
    https://source.android.com/docs/core/graphics/reduce-consumption#enable-backward

--- device.mk
+++ device.mk
@@ -504,7 +504,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=2048 \
     ro.surface_flinger.use_vr_flinger=false \
     ro.surface_flinger.start_graphics_allocator_service=false \
-    ro.surface_flinger.primary_display_orientation=ORIENTATION_0
+    ro.surface_flinger.primary_display_orientation=ORIENTATION_0 \
+    ro.surface_flinger.clear_slots_with_set_layer_buffer=true
 
 # Input resampling configuration
 PRODUCT_PROPERTY_OVERRIDES += \
