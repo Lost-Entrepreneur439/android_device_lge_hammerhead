1edcfd4037ed8ffae2cd7e883d46f8db43c1b122
Yaroslav Zviezda <acroreiser@gmail.com>
2026-02-23 22:09:53 +0200

    hammerhead: use default vm.dirty{_background}_bytes

--- rootdir/init.post_boot.sh
+++ rootdir/init.post_boot.sh
@@ -3,8 +3,6 @@
 # Script to setup many things after boot
 
 echo 3000000 > /proc/sys/kernel/sched_min_granularity_ns
-echo 52428800 > /proc/sys/vm/dirty_bytes
-echo 4194304 > /proc/sys/vm/dirty_background_bytes
 
 echo 100 > /dev/blkio/background/blkio.weight
 
