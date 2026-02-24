ae74a9976609331d192d68d9815755951f9bdbc9
Yaroslav Zviezda <acroreiser@gmail.com>
2026-02-24 20:52:58 +0200

    hammerhead: tune /proc/sys/vm/watermark_scale_factor

--- rootdir/init.post_boot.sh
+++ rootdir/init.post_boot.sh
@@ -7,5 +7,6 @@ echo 3000000 > /proc/sys/kernel/sched_min_granularity_ns
 echo 100 > /dev/blkio/background/blkio.weight
 
 echo 4 > /proc/sys/vm/kswapd_threads
+echo 300 > /proc/sys/vm/watermark_scale_factor
 
 /vendor/bin/timekeep restore
