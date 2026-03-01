422f20362d69cce207500b60bc1bde53af2dd8af
Andrew Hexen <SyberHexen@gmail.com>
2026-03-01 16:09:09 +0200

    hammerhead: disable debug.sf.latch_unsignaled
    * Disabling this helps reduces notification flicker, UI performance is not impacted.
    (per:  change https://android.googlesource.com/platform/frameworks/native/+/c5da271)
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    
    Change-Id: I8b7aed9bc5ff03bd8d1cd05f70c8568c1faf971c

--- device.mk
+++ device.mk
@@ -327,7 +327,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.hw=0 \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     debug.sf.recomputecrop=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
