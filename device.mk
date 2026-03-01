37434f7cbdf8990ca5dddee343cfd407f25418d1
Yaroslav Zviezda <acroreiser@gmail.com>
2026-03-01 15:11:15 +0200

    hammerhead: radio: rebrand radio 1.4 legacy wrapper for hammerhead

--- device.mk
+++ device.mk
@@ -360,7 +360,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0-service \
-    android.hardware.radio@1.4-service.legacy
+    android.hardware.radio@1.4-service.hammerhead
 
 # NTFS Filesystem
 PRODUCT_PACKAGES += \
