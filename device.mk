336bd600104e0977e06c7c0637d60b7227dcfdd4
Eruvaram Kumar Raja Reddy <quic_erajared@quicinc.com>
2026-05-19 16:28:35 -0400

    hammerhead: Disable triple buffer for 1G memory optimization

--- device.mk
+++ device.mk
@@ -342,7 +342,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.color_saturation=1.0 \
     persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1 \
-    ro.launcher.blur.appLaunch=0
+    ro.launcher.blur.appLaunch=0 \
+    ro.sf.disable_triple_buffer=1
 
 # DRM
 PRODUCT_PACKAGES += \
