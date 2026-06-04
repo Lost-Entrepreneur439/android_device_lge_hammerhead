287d1684a53035f56f41ae16d2fee91b20c5e36a
Yaroslav Zviezda <acroreiser@gmail.com>
2026-06-04 15:26:07 -0400

    hammerhead: remove zram swap aggressiveness tuning

--- rootdir/init.post_boot.sh
+++ rootdir/init.post_boot.sh
@@ -6,12 +6,6 @@ echo 3000000 > /proc/sys/kernel/sched_min_granularity_ns
 
 echo 100 > /dev/blkio/background/blkio.weight
 
-# Tune zram swap aggressiveness
-echo 50 > /proc/sys/vm/vfs_cache_pressure
-echo 0 > /proc/sys/vm/extra_free_kbytes
-echo 1000 > /proc/sys/vm/watermark_scale_factor
-echo 150 > /proc/sys/vm/swappiness
-
 echo 4 > /proc/sys/vm/kswapd_threads
 
 # Boost cpus with active kswapd threads
