484d4da5852517e32d996fe5db2de19584884992
Yaroslav Zviezda <acroreiser@gmail.com>
2026-06-10 16:41:03 -0400

    hammerhead: wireless-regdomain: fix gsm.operator.iso-country parsing and dualsim support
    Thanks to @CakesTwix to point me on issue with the script.

--- rootdir/wireless-regdomain.sh
+++ rootdir/wireless-regdomain.sh
@@ -2,5 +2,7 @@
 #
 # Script to set wireless regulatory domain
 
-/vendor/bin/iw reg set $(getprop gsm.sim.operator.iso-country | tr a-z A-Z)
-/vendor/bin/iw reg set $(getprop gsm.operator.iso-country | tr a-z A-Z
+/vendor/bin/iw reg set $(getprop gsm.sim.operator.iso-country | tr a-z A-Z  | sed s/,// | cut -c 1-2)
+/vendor/bin/iw reg set $(getprop gsm.operator.iso-country | tr a-z A-Z | sed s/,// | cut -c 1-2)
+
+exit 0
