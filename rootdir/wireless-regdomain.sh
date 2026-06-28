0881ce8b1b8663f7531271cec9ea590a273a5fcf
Yaroslav Zviezda <acroreiser@gmail.com>
2026-06-28 16:05:54 -0400

    hammerhead: wireless-regdomain.sh: allow to overrride regulatory domain

--- rootdir/wireless-regdomain.sh
+++ rootdir/wireless-regdomain.sh
@@ -5,4 +5,8 @@
 /vendor/bin/iw reg set $(getprop gsm.sim.operator.iso-country | tr a-z A-Z  | sed s/,// | cut -c 1-2)
 /vendor/bin/iw reg set $(getprop gsm.operator.iso-country | tr a-z A-Z | sed s/,// | cut -c 1-2)
 
+if [ "$(getprop persist.wifi.regdomain.override)" != "" ]; then
+	/vendor/bin/iw reg set $(getprop persist.wifi.regdomain.override | tr a-z A-Z)
+fi
+
 exit 0
