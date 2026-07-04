475bd24d39922e36d784f08d1fa5f10b1aaba590
Yaroslav Zviezda <acroreiser@gmail.com>
2026-07-04 08:29:56 -0400

    hammerhead: do not try to set sim-based wireless regdomain when it is overriden by property

--- rootdir/wireless-regdomain.sh
+++ rootdir/wireless-regdomain.sh
@@ -2,11 +2,17 @@
 #
 # Script to set wireless regulatory domain
 
-/vendor/bin/iw reg set $(getprop gsm.sim.operator.iso-country | tr a-z A-Z  | sed s/,// | cut -c 1-2)
-/vendor/bin/iw reg set $(getprop gsm.operator.iso-country | tr a-z A-Z | sed s/,// | cut -c 1-2)
+if [ "$(getprop wifi.regdomain.override.done)" == "1" ]; then
+  exit 0
+fi
 
 if [ "$(getprop persist.wifi.regdomain.override)" != "" ]; then
-	/vendor/bin/iw reg set $(getprop persist.wifi.regdomain.override | tr a-z A-Z)
+  /vendor/bin/iw reg set $(getprop persist.wifi.regdomain.override | tr a-z A-Z)
+  setprop wifi.regdomain.override.done 1
+  exit 0
 fi
+
+/vendor/bin/iw reg set $(getprop gsm.sim.operator.iso-country | tr a-z A-Z  | sed s/,// | cut -c 1-2)
+/vendor/bin/iw reg set $(getprop gsm.operator.iso-country | tr a-z A-Z | sed s/,// | cut -c 1-2)
 
 exit 0
