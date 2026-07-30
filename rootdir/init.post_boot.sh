46805da617f309a8ee1546661aa9db2745ea0374
Yaroslav Zviezda <acroreiser@gmail.com>
2026-07-30 22:18:06 +0300

    hammerhead: stop moving kswapd* to top-app cpuctl
    We are stopped boosting top-app tasks on migration.

--- rootdir/init.post_boot.sh
+++ rootdir/init.post_boot.sh
@@ -8,10 +8,4 @@ echo 100 > /dev/blkio/background/blkio.weight
 
 echo 4 > /proc/sys/vm/kswapd_threads
 
-# Boost cpus with active kswapd threads
-echo $(pgrep kswapd0:0) > /dev/cpuctl/top-app/tasks
-echo $(pgrep kswapd0:1) > /dev/cpuctl/top-app/tasks
-echo $(pgrep kswapd0:2) > /dev/cpuctl/top-app/tasks
-echo $(pgrep kswapd0:3) > /dev/cpuctl/top-app/tasks
-
 /vendor/bin/timekeep restore
